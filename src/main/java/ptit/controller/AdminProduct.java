c7551634006a0893ced8bda8ca3ac1f0628231bc
xdnuos <xdnuos@gmail.com>
2023-03-02 13:32:43 +0700

    test

--- src/main/java/ptit/controller/AdminProduct.java
+++ src/main/java/ptit/controller/AdminProduct.java
@@ -1,7 +1,17 @@
 package ptit.controller;
 
+import java.io.File;
+import java.math.BigDecimal;
+
+import org.apache.commons.io.FilenameUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class AdminProduct {
@@ -13,5 +23,29 @@ public class AdminProduct {
 	@RequestMapping(value = "/admin/addproduct")
 	public String addProduct() {
 		return "admin/addproduct";
-	}
+	}  
+	
+    @PostMapping(value = "admin/addCar")
+    public String addCar(Car car, Model model,
+    		 @RequestParam(value = "carName") String carName,
+    		 @RequestParam(value = "desc") String desc,
+    		 @RequestParam(value = "price") BigDecimal price,
+    		 SessionStatus status,
+    		 @RequestParam("image") MultipartFile image) {
+        try {
+            imgName = image.getOriginalFilename().replace(image.getOriginalFilename(), FilenameUtils.getBaseName(image.getOriginalFilename()).concat(currentDate) + "." + FilenameUtils.getExtension(image.getOriginalFilename())).toLowerCase();
+            File file = new File(this.getFolderUpload(), imgName);
+            image.transferTo(file);
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        
+    	car.setName(carName);
+    	car.setDescription(desc);
+    	car.setPrice(price);
+    	car.setImgName(imgName);
+    	carService.save(car);
+    	status.setComplete();
+        return "redirect:/admin";
+    }
 }
